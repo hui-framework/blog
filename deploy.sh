6d6d25b33d24873eecb6b8cd77163b1fc729d3ee
kawhi66 <kawhi_site@163.com>
2021-06-07 15:43:03 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hui-framework/blog.git gh-pages
+git push -f https://github.com/hui-framework/blog.git master:gh-pages
 
 cd -
