e09ba0ed7152acf900bd60d95fc24f272579323e
kawhi66 <kawhi_site@163.com>
2021-06-07 15:33:16 +0800

    update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  title: 'Mediumish',
-  base: '/vuepress-theme-mediumish/',
-  description: 'Blog example for Mediumish theme with VuePress',
+  title: 'HUI FRAMEWORK',
+  base: '/blog/',
+  description: 'HUI Framework Team Blog',
   logo: './assets/img/logo.png',
   theme: require.resolve('../../'),
   themeConfig: {
-  authors: [
+    authors: [
       {
-      name: 'Sal',
-      avatar: '/assets/img/sal.jpg',
-      link: 'https://wowthemes.net/donate',
-      linktext: 'Follow',
+        name: 'Sal',
+        avatar: '/assets/img/sal.jpg',
+        link: 'https://wowthemes.net/donate',
+        linktext: 'Follow',
       },
       {
         name: 'John Doe',
@@ -22,49 +22,9 @@ module.exports = {
     footer: {
       contact: [
         {
-          type: 'codepen',
-          link: '#',
-        },
-        {
-          type: 'facebook',
-          link: '#',
-        },
-        {
           type: 'github',
-          link: 'https://github.com/wowthemesnet/mediumish-vuepress-blog-theme',
+          link: 'https://github.com/hui-framework',
         },
-        {
-          type: 'gitlab',
-          link: '#',
-        },
-        {
-          type: 'instagram',
-          link: '#',
-        },
-        {
-          type: 'linkedin',
-          link: '#',
-        },
-        {
-          type: 'mail',
-          link: '#',
-        },
-        {
-          type: 'messenger',
-          link: '#',
-        },
-        {
-          type: 'phone',
-          link: '#',
-        },
-        {
-          type: 'twitter',
-          link: 'https://twitter.com/wowthemesnet',
-        },
-        {
-          type: 'web',
-          link: '#',
-        }
       ],
       copyright: [
         {
@@ -73,24 +33,25 @@ module.exports = {
         },
         {
           text: 'Made with Mediumish - free Vuepress theme',
-          link: 'https://bootstrapstarter.com/bootstrap-templates/vuepress-theme-mediumish/',
+          link:
+            'https://bootstrapstarter.com/bootstrap-templates/vuepress-theme-mediumish/',
         },
       ],
     },
 
-    sitemap: {
-      hostname: 'https://github.com/wowthemesnet/vuepress-theme-mediumish/'
-    },
-    comment: {
-      service: 'disqus',
-      shortname: 'demowebsite',
-    },
-    newsletter: {
-      endpoint: 'https://wowthemes.us11.list-manage.com/subscribe/post?u=8aeb20a530e124561927d3bd8&id=8c3d2d214b'
-    },
-    feed: {
-      canonical_base: 'https://github.com/wowthemesnet/vuepress-theme-mediumish/',
-    },
-    smoothScroll: true
+    // sitemap: {
+    //   hostname: 'https://github.com/wowthemesnet/vuepress-theme-mediumish/'
+    // },
+    // comment: {
+    //   service: 'disqus',
+    //   shortname: 'demowebsite',
+    // },
+    // newsletter: {
+    //   endpoint: 'https://wowthemes.us11.list-manage.com/subscribe/post?u=8aeb20a530e124561927d3bd8&id=8c3d2d214b'
+    // },
+    // feed: {
+    //   canonical_base: 'https://github.com/wowthemesnet/vuepress-theme-mediumish/',
+    // },
+    smoothScroll: true,
   },
 }
